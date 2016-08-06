b9b756161f95c5694509ade40164537e0b764e3f
BallsSqueezer <hien.trquang@gmail.com>
2016-08-06 11:55:17 +0700

    Set up project

---
+++ PinGo/HomeTimelineViewController.swift
@@ -0,0 +1,49 @@
+//
+//  HomeTimelineViewController.swift
+//  PinGo
+//
+//  Created by Hien Quang Tran on 8/3/16.
+//  Copyright © 2016 Hien Tran. All rights reserved.
+//
+
+import UIKit
+
+class HomeTimelineViewController: BaseViewController {
+    //MARK: - Outlets and variables
+    
+
+    
+    //MARK: - Load view
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Create gesture to access UserProfileVC by tapping on the profile image
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - Actions
+
+
+    
+    //MARK: - Navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+    
+    //MARK: Helpers
+
+}
+
+/*
+//MARK: - EXTENSION UITableViewDataSource, UITableViewDelegate
+extension HomeTimelineViewController: UITableViewDataSource, UITableViewDelegate {
+    
+}
+ */
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'PinGo' do
+  use_frameworks!
+
+  # Pods for PinGo
+  pod 'GoogleMaps'
+
+end
